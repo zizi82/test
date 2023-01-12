212b5e71cee3c4cb98bd53fe611f52c911e6b0ac
zizi82 <zahra.kosh82@gmail.com>
2023-01-12 15:50:32 +0330

    modify user header

--- z-user.h
+++ z-user.h
@@ -1,4 +1,17 @@
 #ifndef ZUSER_H
 #define ZUSER_H
+#include<string>
+#include<iostream>
+
+using namespace std;
+#define user_table "users.txt"
+
+class registeration{
+public:
+    string username;
+    string password;
+    registeration();
+    string score;
+};
 
 #endif // ZUSER_H
