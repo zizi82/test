60051dd249d3bb3e7e22c4362c9189b72453bb51
zizi82 <zahra.kosh82@gmail.com>
2023-01-12 15:47:30 +0330

    modify main code

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include<iostream>
 #include<string>
 #include<fstream>
-#include"user-pass.h"
-#include "home.h"
+#include"user-log.h"
+#include"z-user.h"
+#include"home.h"
 
 using namespace std;
 
@@ -12,9 +13,20 @@ int main()
 {
 
     wellcome_message();
-    registeration user1;
-   // user1.username_and_password();
-    user1.add_user();
-
+    setting();
+    z_user n;
+    while (true) {
+        commands z = get_commands();
+        switch (z) {
+        case _sign_in: n.login();break;
+        case _registeration: n.adduser();break;
+        case _quit: return 0;
+        case _help: wellcome_message();break;
+        case _jadval:n.score_tb();break;
+        case _setting:setting();break;
+        case _guest_entry:n.guest();break;
+        case _bad_command: cout<<"bad command!"<<endl;break;
+        }
+    }
     return 0;
 }
